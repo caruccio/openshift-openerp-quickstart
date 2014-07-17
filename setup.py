93cf162960568bd13f470e066ed201e6de58a1df
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2014-07-17 14:43:53 -0300

    Using requirementes.txt

--- setup.py
+++ setup.py
@@ -1,43 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      dependency_links = ['http://download.gna.org/pychart/'],
-      install_requires=['gevent',
-                         # OpenERP requires
-                         'pychart',
-                         'babel',
-                         'docutils',
-                         'feedparser',
-                         'gdata',
-                         'Jinja2',
-                         'lxml',
-                         'mako',
-                         'mock',
-                         # We use Pillow instead PIL
-                         'pillow',
-                         # PIL must be installed using pip, not easy_install.
-                         # See https://mail.python.org/pipermail/image-sig/2010-August/006480.html
-                         #'PIL', # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
-                         'psutil', # windows binary code.google.com/p/psutil/downloads/list
-                         'psycopg2 >= 2.2',
-                         'pydot',
-                         'python-dateutil < 2',
-                         #'python-ldap', # optional
-                         'python-openid',
-                         'pytz',
-                         'pywebdav',
-                         'pyyaml',
-                         'reportlab', # windows binary pypi.python.org/pypi/reportlab
-                         'simplejson',
-                         'unittest2',
-                         'vatnumber',
-                         'vobject',
-                         'werkzeug',
-                         'xlwt',
-      ]
+setup(name='OpenERP',
+      version='7.0',
+      description='OpenERP on OpenShift',
+      author='Mateus Caruccio',
+      author_email='mateus.caruccio@getupcloud.com',
+      url='https://github.com/caruccio/openshift-openerp-quickstart',
 )
